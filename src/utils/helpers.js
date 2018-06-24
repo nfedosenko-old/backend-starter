d38b810326d7cf4ef6194773b8cf6fd10dc2b00b
Nikita Fedosenko <nikita.fedosenko@vilmate.com>
2018-06-24 16:38:49 +0300

    fixed issues with link generating

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19,6 +19,10 @@ exports.generateToken = (bytes = 32, encoding = 'base64') => {
     });
 };
 
+exports.generateRedirectLinkWithToken = (headers, page, token) => {
+    return `${headers.host}/${page}?token=${token}`;
+};
+
 exports.validateEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
